cb79d1e679ac750074e8c76656dc61c816c9e4ce
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 01:13:28 +0530

    Add project files.

---
+++ AngularJsDemonstration_3/Scripts/demoAppScripts.js
@@ -0,0 +1,53 @@
+﻿/// <reference path="angular.js" />
+var app = angular.module('demoApp', []);
+
+app.filter('YesNo', function () {
+
+    return function (input, sal,name) {
+        console.log('Name is : '+name+' Salary is : ' + sal);
+
+        if (input) {
+            return 'Yes';
+        }
+        else
+        {
+            return 'No';
+        }
+    };
+
+});
+
+app.controller('demoCntrl', function ($scope,$filter,$interval) {
+
+    $scope.employeeList = [
+        { Id: 101, Name: 'Abhinav', Location: 'Bangalore', Salary: 12345, IsContract:true },
+        { Id: 102, Name: 'Abhishek', Location: 'Chennai', Salary: 10000, IsContract: false },
+        { Id: 103, Name: 'Ajay', Location: 'Bangalore', Salary: 34567, IsContract: true },
+        { Id: 104, Name: 'Anil', Location: 'Bangalore', Salary: 5000, IsContract: false },
+        { Id: 105, Name: 'Anirudh', Location: 'Chennai', Salary: 56789, IsContract: true },
+        { Id: 106, Name: 'Akash', Location: 'Bangalore', Salary: 67890, IsContract: false },
+        { Id: 107, Name: 'Akshay', Location: 'Bangalore', Salary: 78900, IsContract: true }
+    ];
+
+    $interval(addNewRecordEachTime, 5000,5);
+
+    function addNewRecordEachTime() {
+        var getId = $scope.employeeList[$scope.employeeList.length - 1].Id + 1;
+
+        $scope.employeeList.push({
+            Id: getId, Name: 'New Name ', Location: getId % 2 == 0 ? 'Bangalore' : 'Chennai',
+            Salary:44444,IsContract:getId%2==0
+        });
+    }
+    $scope.searchEmployee = function () {
+        //var empArray = $filter('filter')($scope.employeeList, { Id: $scope.empId });
+
+        //if (empArray!=undefined && empArray!=null && empArray.length > 0) {
+        //    console.log(empArray[0]);
+        //}
+
+        var empArray = $filter('filter')($scope.employeeList, { Location: 'Bangalore', IsContract:true });
+
+        console.log(empArray);
+    };
+});
---
+++ AngularJsDemonstration_3/Scripts/testAppScripts.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="angular.js" />
+var app = angular.module('testApp', []);
+
+app.factory('arithOpsFactory', function () {
+
+    var myFac = {};
+
+    myFac.add = function (a, b) {
+        return a + b;
+    };
+
+    myFac.sub = function (a, b) {
+        return a - b;
+    };
+
+    myFac.mul = function (a, b) {
+        return a * b;
+    };
+
+    myFac.div = function (a, b) {
+        return a / b;
+    };
+
+    return myFac;
+});
+
+app.controller('testCntrl', function ($scope, arithOpsFactory) {
+
+    $scope.arithOps = function (i) {
+        switch (i) {
+            case 1: var result = arithOpsFactory.add($scope.firstNumber, $scope.secondNumber);
+                alert("Addition Result is : " + result);
+                break;
+            case 2: var result = arithOpsFactory.sub($scope.firstNumber, $scope.secondNumber);
+                alert("Subtraction Result is : " + result);
+                break;
+            case 3: var result = arithOpsFactory.mul($scope.firstNumber, $scope.secondNumber);
+                alert("Multiplication Result is : " + result);
+                break;
+            case 4: var result = arithOpsFactory.div($scope.firstNumber, $scope.secondNumber);
+                alert("Division Result is : " + result);
+                break;
+        }
+    };
+
+});
